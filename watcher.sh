d7887b058fe10e2cc1811bb60130f18bc6a2abc3
subhashkumarBabu <subhashkp@hotmail.com>
2024-04-22 17:55:13 +0000

    modified watcher

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,30 @@
-#TODO
+#!/bin/bash
+
+# Name of the namespace
+NAMESPACE="sre"
+
+# Name of the deployment
+DEPLOYMENT="swype-app"
+
+# Maximum number of restarts before scaling down
+MAX_RESTARTS=3
+
+while true; do
+  # Get the number of restarts of the pod
+  RESTARTS=$(kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
+
+  echo "Current number of restarts: ${RESTARTS}"
+
+  # If the number of restarts is greater than the maximum allowed, scale down the deployment
+  if (( RESTARTS > MAX_RESTARTS )); then
+    echo "Maximum number of restarts exceeded. Scaling down the deployment..."
+    kubectl scale --replicas=0 deployment/${DEPLOYMENT} -n ${NAMESPACE}
+    break
+  else
+        # Step 7: Pause
+        echo "Number of restarts is within limit. Pausing for 60 seconds."
+        sleep 60
+  fi
+
+
+done
